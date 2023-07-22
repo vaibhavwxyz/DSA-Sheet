b7ee1d9a22561040390db20b750c9e227810d2b6
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-07-22 22:54:00 +0530

    dsa

--- 01- Problems on Arrays/14- Find all repeating elements in an Array/repeatingElements.js
+++ 01- Problems on Arrays/14- Find all repeating elements in an Array/repeatingElements.js
@@ -1,12 +1,22 @@
 function findRepeatingElements(arr) {
-  let tempArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length; j++) {
+  let cnt = 0;
+  let n = arr.length;
+  let dup = [];
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
       if (arr[i] === arr[j]) {
-        tempArr[]
+        dup[cnt++] = arr[i];
       }
     }
   }
+
+  let output = "The repeating elements are: ";
+  for (let i = 0; i < cnt; i++) {
+    if (dup[i] !== dup[i + 1]) {
+      output += dup[i] + " ";
+    }
+  }
+  console.log(output);
 }
 
 let arr = [1, 1, 2, 3, 4, 4, 5, 2];
